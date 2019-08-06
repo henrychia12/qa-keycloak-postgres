f957f47ec83d8853bcebed9a231e8143ef509684
henrychia12 <henry_chia21@hotmail.co.uk>
2019-08-06 09:50:43 +0000

    ssl script works now

--- ssl-setup.sh
+++ ssl-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # bypass the keycloak HTTPS required issue when deployed to remote server
-docker exec -it qa-keycloak-postgres_keycloak_1 psql -U keycloak -d keycloak -c "update realm set ssl_required='NONE'";
+docker exec -it qa-keycloak-postgres_postgres_1 psql -U keycloak -d keycloak -c "update realm set ssl_required='NONE'";
 
 # restart keycloak
 docker restart qa-keycloak-postgres_keycloak_1
